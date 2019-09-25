1df03a1ac6c654d49bdd5f6768d33c9fd7de42c3
Romain Bardoux <r.bardouxx@hotmail.fr>
2019-09-25 14:06:47 +0200

    liens nav maj

--- scriptnav.php
+++ scriptnav.php
@@ -1,10 +1,10 @@
 <nav class="navbar">
     <button class="nav__burger"><img src="Images/Image-acceuil/Hamburger_icon.png" alt="Menu"></button>
     <ul class="nav__list">
-        <li><a href="accueil.html">ACCUEIL</a></li>
-        <li><a href="numerique.html">ECOSYSTEME DU NUMERIQUE</a></li>
-        <li><a href="economie.html">DYNAMISME ECONOMIQUE</a></li>
-        <li><a href="lavie.html">QUALITE DE VIE</a></li>
-        <li><a href="contacts.html">CONTACT</a></li>
+        <li><a href="accueil.php">ACCUEIL</a></li>
+        <li><a href="numerique.php">ECOSYSTEME DU NUMERIQUE</a></li>
+        <li><a href="economie.php">DYNAMISME ECONOMIQUE</a></li>
+        <li><a href="lavie.php">QUALITE DE VIE</a></li>
+        <li><a href="contacts.php">CONTACT</a></li>
     </ul>
 </nav>
